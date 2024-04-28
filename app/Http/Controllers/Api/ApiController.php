3cee5301b4ccfff5e93642edffb51506d9284bcb
Majumder99 <souravmajumderk99@gmail.com>
2024-04-28 18:01:09 +0600

    (crud)[snactum]: crud operation done

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -4,22 +4,91 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class ApiController extends Controller {
     //Register api, login api, profile api, logout api
     // POST[name,email,password] /api/register
     public function register(Request $request) {
+        // Validation
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+        // User
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return response()->json([
+            "status" => true,
+            "message" => "User created successfully",
+            "data" => [
+                "name" => $request->name,
+                "email" => $request->email,
+            ]
+        ]);
     }
 
     // POST[email,password] /api/login
     public function login(Request $request) {
+        //  validation
+        $request->validate([
+            'email' => 'required|email|string',
+            'password' => 'required',
+        ]);
+        $user = User::where("email", $request->email)->first();
+        if (!empty($user)) {
+            // user found
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken("auth_token")->plainTextToken;
+                return response()->json([
+                    "status" => true,
+                    "message" => "User logged in successfully",
+                    "data" => [
+                        "name" => $user->name,
+                        "email" => $user->email,
+                    ],
+                    "token" => $token
+                ]);
+            } else {
+                return response()->json([
+                    "status" => false,
+                    "message" => "Password not matched",
+                    "data" => []
+                ]);
+            }
+        } else {
+            return response()->json([
+                "status" => false,
+                "message" => "User not found",
+                "data" => []
+            ]);
+        }
     }
 
     // GET [Auth: Token] /api/profile
     public function profile() {
+        $userData = auth()->user();
+        return response()->json([
+            "status" => true,
+            "message" => "User profile",
+            "data" => $userData,
+            "id" => auth()->user()->id
+        ]);
     }
 
     // GET [Auth: Token] /api/logout
     public function logout() {
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            "status" => true,
+            "message" => "User logged out successfully",
+            "data" => []
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiController;
+
+// open routes
+Route::post("register", [ApiController::class, "register"]);
+Route::post("login", [ApiController::class, "login"]);
+
+// protected routes
+Route::group([
+    "middleware" => ["auth:sanctum"]
+], function () {
+    Route::get("profile", [ApiController::class, "profile"]);
+    Route::get("logout", [ApiController::class, "logout"]);
+});
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
